ab2cbbd6e64cad72e92157471ce48a4e2d05e4bd
Jean <acalto2@gmail.com>
2022-12-25 00:16:12 +0100

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ios_reminders/models/todo_list/todo_list_collection.dart';
 import 'package:ios_reminders/screens/add_reminder/add_reminder_screen.dart';
@@ -5,11 +6,27 @@ import 'package:ios_reminders/screens/add_reminder/add_reminder_screen.dart';
 import 'package:ios_reminders/screens/home/addList/add_list_screen.dart';
 import 'package:ios_reminders/screens/home/home_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
-void main(List<String> args) {
-  runApp(
-    const App(),
+//void main(List<String> args) async {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kDebugMode) {
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
